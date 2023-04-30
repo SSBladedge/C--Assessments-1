66e2bef7f81264d23103cbe54f95e568514a4307
George <gbemidale.a@gmail.com>
2023-04-30 11:31:24 +0100

    C# Project

---
+++ AsmtLib/AsmtLib.cs
@@ -0,0 +1,87 @@
+namespace AsmtLib
+{
+    public class Lib
+    {
+        private static List<int> GetList()
+        {
+            Console.WriteLine("Please input 5 digits for your list");
+            List<int> userInts = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                string? userVal = Console.ReadLine();
+                int userInt;
+                if (int.TryParse(userVal, out userInt))
+                    userInts.Add(userInt);
+            }
+            return userInts;
+        }
+
+        public static void LargestNumber()
+        {
+
+            List<int> arr = GetList();
+            if (arr == null || arr.Count == 0)
+            {
+                Console.WriteLine("Please add values to the list");
+            }
+
+            else
+            {
+                int largest = arr[0];
+
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    if (arr[i] >= largest)
+                        largest = arr[i];
+                }
+
+                Console.WriteLine($"Largest is {largest}");
+            }
+        }
+
+        public static void RevList()
+        {
+            List<int> arr = GetList();
+            if (arr == null || arr.Count == 0)
+            {
+                Console.WriteLine("Please add values to the list");
+            }
+
+            else
+            {
+                int c = arr.Count;
+                for (int i = 0; i < c / 2; i++)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[c - i - 1];
+                    arr[c - i - 1] = temp;
+                }
+                Console.WriteLine(String.Join(',', arr));
+            }
+        }
+
+        public static void ChkList()
+        {
+            //FOR USERS - REPLACE GETLIST() BELOW WITH A COPY OF THE DESIRED LIST TO PARSE THROUGH
+
+            List<int> arr = GetList();
+            if (arr == null || arr.Count == 0)
+            {
+                Console.WriteLine("Please add values to the list");
+            }
+
+            else
+            {
+                Console.WriteLine("Please enter a value to search for in the list: ");
+                string? srchVal = Console.ReadLine();
+
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
+
+
---
+++ README.txt
@@ -0,0 +1,7 @@
+Hi, Thank you for previewing my C# assessment programs 
+
+Please use the UI for full access to all the programs created for this project 
+
+Please leave any constructive criticism you can as it will help me further improve on my skills in programming 
+
+Your participation is greatly appreciated. 
